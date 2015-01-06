93a65763b45325cfd11158394ffc7dcad7238036
Tewodros Wondimu <tewodros.wr@gmail.com>
2015-01-06 15:14:00 -0800

    Initial Commit

---
+++ firstGitApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstGitApp
+//
+//  Created by Tewodros Wondimu on 1/6/15.
+//  Copyright (c) 2015 Tewodros Wondimu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ firstGitApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  firstGitApp
+//
+//  Created by Tewodros Wondimu on 1/6/15.
+//  Copyright (c) 2015 Tewodros Wondimu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
